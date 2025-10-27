b8a0fef85a3d6ff40df07f54c7f083de86e96db7
theeratham Meekaew <119734782+theetham@users.noreply.github.com>
2025-10-28 05:08:52 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { router as user } from "./controller/user";
-import purchasesRouter from '/web_A/Web-GameTopUp-main/node-expres-mysql-webgame/controller/purchases';
+import { router as purchasesRouter } from './controller/purchases';
 import { router as game } from "./controller/game";
 
 import bodyParser from "body-parser";
@@ -15,3 +15,4 @@ app.use("/user", user);
 app.use("/game", game);
 app.use('/purchases', purchasesRouter);
 app.use("/uploads", express.static("uploads"));
+
